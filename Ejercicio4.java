5e69b84d3d4bea2a428481fe7b68133e23f5c755
ddealsan <ddealsan@myuax.com>
2024-02-07 19:05:08 +0100

    Avance

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,2 +1,11 @@
-public class Ejercicio4 {
-}
+import java.util.Scanner;
+
+public class MinMaxListaNumeros {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce una lista de números enteros positivos. Introduce un número negativo para terminar.");
+
+        // Variables para almacenar el mínimo y el máximo
+        int minimo = Integer.MAX_VALUE;
+        int maximo = Integer.MIN_VALUE;
