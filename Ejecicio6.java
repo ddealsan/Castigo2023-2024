f267b38d7a05bd2b920abe7db8c9e279ffd2b3c2
ddealsan <ddealsan@myuax.com>
2024-02-07 19:08:51 +0100

    Avance

--- Ejecicio6.java
+++ Ejecicio6.java
@@ -25,3 +25,15 @@ public class NumerosPrimos {
 
         return primos;
     }
+    public static boolean esPrimo(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
