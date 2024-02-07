d35d8c525767db2277c45a60bffd540c4cb2042d
ddealsan <ddealsan@myuax.com>
2024-02-07 19:15:03 +0100

    Avance

--- Ejercicio9.java
+++ Ejercicio9.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ProductoEscalar {
+public class Ejercicio9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
