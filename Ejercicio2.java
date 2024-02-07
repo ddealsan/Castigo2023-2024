01ada905bf58cd44af9f50de9b28c5cf0b7053a7
ddealsan <ddealsan@myuax.com>
2024-02-07 18:45:10 +0100

    Avance

---
+++ Ejercicio2.java
@@ -0,0 +1,5 @@
+public class MaximosEnteros {
+    public static void main(String[] args) {
+
+        char mayorChar = Character.MAX_VALUE;
+        System.out.println("El mayor número entero representable en un char es: " + (int) mayorChar);
