64f999464882f2aee2147347ed55aaa3bfe0c37d
ddealsan <ddealsan@myuax.com>
2024-02-07 19:11:05 +0100

    Avance

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -1,2 +1,16 @@
-public class Ejercicio7 {
-}
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class FactoresPrimos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Leer un número entero desde el teclado
+        System.out.print("Introduce un número entero positivo: ");
+        int numero = scanner.nextInt();
+
+        // Descomponer el número en factores primos
+        ArrayList<Integer> factoresPrimos = descomponerEnFactoresPrimos(numero);
+
+        // Imprimir los factores primos
+        System.out.print(numero + " = ");
