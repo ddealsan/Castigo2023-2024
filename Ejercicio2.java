9f4d03981c35e944edf1e956830b3cc6395fd039
ddealsan <ddealsan@myuax.com>
2024-02-07 18:46:21 +0100

    Avance

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -5,3 +5,6 @@ public class MaximosEnteros {
         System.out.println("El mayor número entero representable en un char es: " + (int) mayorChar);
         short mayorShort = Short.MAX_VALUE;
         System.out.println("El mayor número entero representable en un short es: " + mayorShort);
+        int mayorInt = Integer.MAX_VALUE;
+        System.out.println("El mayor número entero representable en un int es: " + mayorInt);
+    }
