38259d02a230a995cc596223c61dee0b13d24a42
ddealsan <ddealsan@myuax.com>
2024-02-07 18:52:35 +0100

    Avance

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -2,3 +2,7 @@ import java.util.ArrayList;
 
 public class SumaMultiplosDe5 {
     public static void main(String[] args) {
+        int[] multiplosDe5 = obtenerMultiplosDe5(1, 100);
+        int suma = calcularSuma(multiplosDe5);
+        int cantidad = multiplosDe5.length;
+    }
