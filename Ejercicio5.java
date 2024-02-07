d000bbfb1d94548d891943180378b19bbbec218c
ddealsan <ddealsan@myuax.com>
2024-02-07 19:06:53 +0100

    Avance

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -1,2 +1,4 @@
-public class Ejercicio5 {
-}
+public class TablasDeMultiplicar {
+    public static void main(String[] args) {
+        // Definir el número de tablas de multiplicar a generar
+        int n = 10;
