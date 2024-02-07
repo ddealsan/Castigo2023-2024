4c77e8e208b72b719346fb27dc48b055444d6781
ddealsan <ddealsan@myuax.com>
2024-02-07 19:16:11 +0100

    Avance

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -13,3 +13,26 @@ public class MatrizSimetrica {
 
         scanner.close();
     }
+    public static void generarYMostrarMatrizSimetrica(int dimension) {
+        // Crear la matriz cuadrada
+        int[][] matriz = new int[dimension][dimension];
+
+        // Rellenar la matriz con números aleatorios
+        for (int i = 0; i < dimension; i++) {
+            for (int j = i; j < dimension; j++) {
+                int numeroAleatorio = (int) (Math.random() * 101); // Números aleatorios entre 0 y 100
+                matriz[i][j] = numeroAleatorio;
+                matriz[j][i] = numeroAleatorio; // Para que la matriz sea simétrica
+            }
+        }
+
+        // Imprimir la matriz por pantalla
+        System.out.println("La matriz simétrica generada es:");
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
