e5f83919a73c28c9a83862a7f96d5912949f9a16
ddealsan <ddealsan@myuax.com>
2024-02-07 18:54:13 +0100

    Avance

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -5,4 +5,11 @@ public class SumaMultiplosDe5 {
         int[] multiplosDe5 = obtenerMultiplosDe5(1, 100);
         int suma = calcularSuma(multiplosDe5);
         int cantidad = multiplosDe5.length;
+        System.out.println("Los múltiplos de 5 comprendidos entre 1 y 100 son:");
+        for (int multiplo : multiplosDe5) {
+            System.out.println(multiplo);
+        }
+        System.out.println("La suma de los múltiplos de 5 es: " + suma);
+        System.out.println("Cantidad de múltiplos de 5: " + cantidad);
+    }
     }
