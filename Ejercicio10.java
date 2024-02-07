1ad237779c75e5b77457120e75b36786a79b635d
ddealsan <ddealsan@myuax.com>
2024-02-07 19:15:58 +0100

    Avance

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -1,2 +1,15 @@
-public class Ejercicio10 {
-}
+import java.util.Scanner;
+
+public class MatrizSimetrica {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Pedir al usuario la dimensión de la matriz cuadrada
+        System.out.print("Introduce la dimensión de la matriz cuadrada: ");
+        int dimension = scanner.nextInt();
+
+        // Llamar a la función para generar y mostrar la matriz simétrica
+        generarYMostrarMatrizSimetrica(dimension);
+
+        scanner.close();
+    }
